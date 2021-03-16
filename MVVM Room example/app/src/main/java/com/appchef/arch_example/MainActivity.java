a1b8e4e4cd669ee078e4e5a8d720069390c9c92a
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-03-16 13:51:48 +0530

    Repository added to Activity

--- MVVM Room example/app/src/main/java/com/appchef/arch_example/MainActivity.java
+++ MVVM Room example/app/src/main/java/com/appchef/arch_example/MainActivity.java
@@ -1,14 +1,32 @@
 package com.appchef.arch_example;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // this method knows the scope of the Activity thus avoid memory leaks also
+        noteViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update the recycler view here.
+                Toast.makeText(MainActivity.this, "OnChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
